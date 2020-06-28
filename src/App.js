fdc82a853be106cf4984b8f30f654030c4b2cac3
Siddhant2505 <siddhant2505@gmail.com>
2020-06-28 15:48:56 +0530

    FIXED HOMEPAGE

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect } from "react";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -44,50 +43,6 @@ function App() {
       </ThemeContextProvider>
     </UserProvider>
   );
-=======
-import React, { useEffect } from 'react';
-
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import HomePage from './components/screens/HomePage';
-import SignInPage from './components/screens/auth/SignInPage';
-import AboutPage from './components/screens/AboutPage';
-import SignUpPage from './components/screens/auth/SignUpPage';
-import UserProvider from './providers/UserProvider';
-import MenuScreen from './components/screens/menu/MenuScreen';
-import { auth } from './firebase/firebase';
-import Navbar from './components/layout/navbar/Navbar';
-import ThemeContextProvider from './contexts/ThemeContext';
-
-function App() {
-	return (
-		<UserProvider>
-			<ThemeContextProvider>
-				<Router>
-					<div className="App" style={{ display: 'flex', flexDirection: 'column' }}>
-						<Navbar />
-						<Switch>
-							<Route path="/signin">
-								<SignInPage />
-							</Route>
-							<Route path="/signup">
-								<SignUpPage />
-							</Route>
-							<Route path="/about">
-								<AboutPage />
-							</Route>
-							<Route path="/menu">
-								<MenuScreen />
-							</Route>
-							<Route path="/">
-								<HomePage />
-							</Route>
-						</Switch>
-					</div>
-				</Router>
-			</ThemeContextProvider>
-		</UserProvider>
-	);
->>>>>>> 20ad2f3f02711da41fff6aca8affcc04dd04b736
 }
 
 export default App;
